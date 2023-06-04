d8cc746fbf673fe1859478c779255e9e893345ac
Qiang Wang <wang-q@outlook.com>
2023-06-05 01:14:16 +0800

    Produce rep.lst and strains.taxon.tsv in finish.sh

--- templates/ass_finish.tera.sh
+++ templates/ass_finish.tera.sh
@@ -5,6 +5,13 @@
 #----------------------------#
 log_warn finish.sh
 
+log_info "ASMs passed the N50 check"
+tsv-join \
+    collect.csv \
+    --delimiter "," -H --key-fields 1 \
+    --filter-file n50.pass.csv \
+    > collect.pass.csv
+
 log_info "Strains without protein annotations"
 cat url.tsv |
     parallel --colsep '\t' --no-run-if-empty --linebuffer -k -j 4 '
@@ -18,18 +25,30 @@ cat url.tsv |
     tsv-uniq \
     > omit.lst
 
-log_info "ASMs passed the N50 check"
-tsv-join \
-    collect.csv \
-    --delimiter "," -H --key-fields 1 \
-    --filter-file n50.pass.csv \
-    > collect.pass.csv
+log_info "Representative or reference strains"
+cat collect.pass.csv |
+    tsv-filter -H -d, --not-empty "RefSeq_category" |
+    tsv-select -H -d, -f name |
+    sed '1d' \
+    > rep.lst
+
+log_info "Strains.taxon.tsv"
+cat collect.pass.csv |
+    sed -e '1d' |
+    tr "," "\t" |
+    tsv-select -f 1,3 |
+    nwr append stdin -c 2 -r species -r genus -r family -r order -r class \
+    > strains.taxon.tsv
 
 log_info "Counts of lines"
 printf "#item\tcount\n" \
     > counts.tsv
 
-for FILE in url.tsv check.lst collect.csv n50.tsv n50.pass.csv omit.lst collect.pass.csv; do
+for FILE in \
+    url.tsv check.lst collect.csv \
+    n50.tsv n50.pass.csv collect.pass.csv \
+    omit.lst rep.lst strains.taxon.tsv \
+    ; do
     cat ${FILE} |
         wc -l |
         FILE=${FILE} perl -nl -MNumber::Format -e '
