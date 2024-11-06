b9344a21be4c629c965a8afc1a19aa0d121a049c
Qiang Wang <wang-q@outlook.com>
2024-11-07 01:31:54 +0800

    Update mh_nr.tera.sh

--- templates/mh_nr.tera.sh
+++ templates/mh_nr.tera.sh
@@ -9,18 +9,63 @@ ANI_VALUE={{ mh_ani_nr }}
 
 log_info Non-redundant strains
 
+cat species.tsv |
+{% for i in ins -%}
+    tsv-join -f ../{{ i }} -k 1 |
+{% endfor -%}
+{% for i in not_ins -%}
+    tsv-join -e -f ../{{ i }} -k 1 |
+{% endfor -%}
+    tsv-select -f 2 |
+    tsv-uniq \
+    > species.lst
+
 cat species.lst |
 while read SPECIES; do
     log_debug "${SPECIES}"
 
-    # Number of assemblies >= 2
-    if [[ ! -s "${SPECIES}/mash.dist.tsv" ]]; then
+    if [[ -f "${SPECIES}/NR.lst" ]]; then
         continue
     fi
 
+    mkdir -p "${SPECIES}"
+
+    cat species.tsv |
+{% for i in ins -%}
+    tsv-join -f ../{{ i }} -k 1 |
+{% endfor -%}
+{% for i in not_ins -%}
+    tsv-join -e -f ../{{ i }} -k 1 |
+{% endfor -%}
+        tsv-filter --str-eq "2:${SPECIES}" |
+        tsv-select -f 1 \
+        > "${SPECIES}/assembly.lst"
+
+    # Number of assemblies >= 2
+    N_ASM=$(
+        cat "${SPECIES}/assembly.lst" | wc -l
+    )
+    if [[ $N_ASM -lt 2 ]]; then
+        continue
+    fi
+
+    echo >&2 "    mash distances"
+    cat "${SPECIES}/assembly.lst" |
+        parallel --no-run-if-empty --linebuffer -k -j 1 "
+            if [[ -e ${SPECIES}/msh/{}.msh ]]; then
+                echo ${SPECIES}/msh/{}.msh
+            fi
+        " \
+        > "${SPECIES}/msh.lst"
+
+    if [[ ! -s "${SPECIES}/mash.nr.tsv" ]]; then
+        mash triangle -E -p 8 -l "${SPECIES}/msh.lst" |
+        tsv-filter --ff-str-ne 1:2 --le "3:${ANI_VALUE}" \
+            > "${SPECIES}/mash.nr.tsv"
+    fi
+
     echo >&2 "    Finding redundants"
-    cat "${SPECIES}/mash.dist.tsv" |
-        tsv-filter --ff-str-ne 1:2 --le "3:${ANI_VALUE}" |
+    cat "${SPECIES}/mash.nr.tsv" |
         hnsm cluster stdin --mode cc \
         > "${SPECIES}/RED.cc.tsv"
 
