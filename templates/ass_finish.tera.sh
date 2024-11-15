5b0fa6c4fa674048bc6ced70e5cdd7b69090497a
Qiang Wang <wang-q@outlook.com>
2024-11-16 00:15:25 +0800

    Update ass_finish.tera.sh

--- templates/ass_finish.tera.sh
+++ templates/ass_finish.tera.sh
@@ -67,9 +67,7 @@ for FILE in \
         datamash check |
         FILE=${FILE} perl -nl -MNumber::Format -e '
             m/(\d+)\s*lines?.+?(\d+)\s*fields?/ or next;
-            printf qq($ENV{FILE}\t%s\t%s\n),
-                Number::Format::format_number($2, 0,),
-                Number::Format::format_number($1, 0,);
+            printf qq($ENV{FILE}\t%s\t%s\n), $2, $1;
             ' \
         >> counts.tsv
 done
